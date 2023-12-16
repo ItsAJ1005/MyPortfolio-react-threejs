b43529e2159a8b4426405bce84038ffa2ac9d9e0
AJ Harsh Vardhan <122255411+ItsAJ1005@users.noreply.github.com>
2023-12-16 17:17:48 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React from "react"
 import {
   VerticalTimeline,
   VerticalTimelineElement,
-} from "react-vertical-timeline-component";
-import { motion } from "framer-motion";
+} from "react-vertical-timeline-component"
+import { motion } from "framer-motion"
 
-import "react-vertical-timeline-component/style.min.css";
+import "react-vertical-timeline-component/style.min.css"
 
-import { styles } from "../styles";
-import { experiences } from "../constants";
-import { SectionWrapper } from "../hoc";
-import { textVariant } from "../utils/motion";
+import { styles } from "../styles"
+import { experiences } from "../constants"
+import { SectionWrapper } from "../hoc"
+import { textVariant } from "../utils/motion"
 
 const ExperienceCard = ({ experience }) => {
   return (
@@ -79,7 +79,7 @@ const Experience = () => {
         </VerticalTimeline>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default SectionWrapper(Experience, "work");
+export default SectionWrapper(Experience, "work")
